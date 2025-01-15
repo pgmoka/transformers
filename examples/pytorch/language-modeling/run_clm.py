5638d195b4bbd70777e59746faf4b3b125fe2161
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2025-01-15 10:40:19 -0800

    not only save on rank-0

--- examples/pytorch/language-modeling/run_clm.py
+++ examples/pytorch/language-modeling/run_clm.py
@@ -693,7 +693,7 @@ def main():
 
         trainer.log_metrics("train", metrics)
         trainer.save_metrics("train", metrics)
-        trainer.save_state()
+        # trainer.save_state()
 
     # Evaluation
     if training_args.do_eval:
